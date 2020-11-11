e00765421c7b21b2baae906376a8153243774b2e
Rodrigo Silva <rodrigosndsilva@gmail.com>
2020-11-11 13:38:40 +0000

    changes on main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
+// g++ -std=c++14 -Wall -Wextra -g *.cpp -o main
+
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-	std::cout << "Hello world!" << std::endl;
-}
+int main(int argc, char *argv[]) { std::cout << "Hello world!" << std::endl; }
